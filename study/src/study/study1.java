eb7702c8dbb56ac5b694b69e300721f6e62ea452
dlekgml <dlekgml0610@naver.com>
2023-12-18 10:28:04 +0900

    배열

--- study/src/study/study1.java
+++ study/src/study/study1.java
@@ -3,8 +3,14 @@ package study;
 public class study1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// 자바스크립트 - let a = new Array();
+		// 			  let b = [10,20,30,40];
+		// 		  a.push(3123); b,push(233);
+		
+		int[] arr1 = {101,20};
+		
+		System.out.println(arr1[0]);
 	}
 
 }
